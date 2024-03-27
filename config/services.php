a1f374e66f16d89b8305aba344f20e62bec9666b
Saeful Mu'minin <saeful2026027@gmail.com>
2024-03-27 08:48:13 +0700

    cek

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'midtrans' => [
         'serverKey'     => env('MIDTRANS_SERVERKEY'),
         'clientKey'     => env('MIDTRANS_CLIENTKEY'),
-        'isProduction'  => env('MIDTRANS_IS_PRODUCTION', true),
+        'isProduction'  => env('MIDTRANS_IS_PRODUCTION', false),
         'isSanitized'   => env('MIDTRANS_IS_SANITIZED', true),
         'is3ds'         => env('MIDTRANS_IS_3DS', true),
     ],
